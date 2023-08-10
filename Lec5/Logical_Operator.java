075c873ace90bb992e2a208fe1cffe0997242879
Mayank Singh <114311263+ESMayank@users.noreply.github.com>
2023-08-11 00:13:28 +0530

    Add files via upload

---
+++ Lec5/Logical_Operator.java
@@ -0,0 +1,11 @@
+package Lec5;
+
+public class Logical_Operator {
+public static void main(String[] args) {
+//	&& --> And logical Operator
+//	|| --> Or Logical Operator
+//	! --> Not operator
+	System.out.println(true&&true);
+	System.out.println(true && 10/0>0);
+}
+}
---
+++ Lec5/String_Concatination.java
@@ -0,0 +1,10 @@
+package Lec5;
+
+public class String_Concatination {
+public static void main(String[] args) {
+	int x = 10,y=20,z=30;
+	String s = "ISRO";
+	s = z+10+s+x+y;
+	System.out.println(s);
+}
+}
