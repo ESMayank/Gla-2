be0c4cc0dc596ad00bf4e070b98b18a3175ab831
Mayank Singh <114311263+ESMayank@users.noreply.github.com>
2023-08-20 00:58:17 +0530

    Add files via upload
    Classes And Object

---
+++ Lec9/Define_Object.java
@@ -0,0 +1,11 @@
+package Lec9;
+
+public class Define_Object {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1;
+//		System.out.println(e1);//gives error
+	}
+
+}
---
+++ Lec9/Guess_The_Output.java
@@ -0,0 +1,14 @@
+package Lec9;
+
+public class Guess_The_Output {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e = new Employee();
+		
+
+			Guess_The_Output t = new Guess_The_Output();
+//			System.out.println(t.e);//gives Error
+	}
+
+}
